d49efec40a668667171eba6b1ca04d65f0604fd9
namihaha77 <47521419+namihaha77@users.noreply.github.com>
2020-02-19 22:29:08 +0400

    solve the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
+  const str = String(n);
+  let result = '';
+  if (str[0] === '-') {
+    result = str.substr(1).split('').reverse().join('');
+  } else {
+      result = str.split('').reverse().join('');
+  }
   
+  return Number(result);
 }
